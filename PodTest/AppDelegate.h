7db68e7287510df291fbb4ebf56b416352a0bc1f
ccuizheng <13063478852@163.com>
2018-09-21 09:16:15 +0800

    第一次提交

---
+++ PodTest/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PodTest
+//
+//  Created by 易服 on 2018/9/21.
+//  Copyright © 2018年 易服. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PodTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PodTest
+//
+//  Created by 易服 on 2018/9/21.
+//  Copyright © 2018年 易服. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
